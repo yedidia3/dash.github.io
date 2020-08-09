d1c1fed9c0769c4b66af361ee1ee34504bfbbfd8
elladia <68752179+elladia@users.noreply.github.com>
2020-08-08 20:30:48 -0500

    new

---
+++ js/keyboard.js
@@ -0,0 +1,146 @@
+
+_createKeys(){
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+        "あ", "い", "う", "え", "お", "ア", "イ", "ウ", "エ", "オ", "か", "き","く", "け", "こ", "カ", "キ", "ク", "ケ", "コ", "さ", "し", "す", "せ", "そ", "サ", "シ", "ス", "セ", "ソ", "た", "ち", "つ", "て", "と", "タ", "チ", "ツ", "テ", "ト", "な", "に","ぬ", </button>
+            <button type="button" class="keyboard__key">ね</button>
+            <button type="button" class="keyboard__key">の</button>
+            <button type="button" class="keyboard__key">ナ</button>
+            <button type="button" class="keyboard__key">ニ</button>
+            <button type="button" class="keyboard__key">ヌ</button>
+            <button type="button" class="keyboard__key">ネ</button>
+            <button type="button" class="keyboard__key">ノ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">は</button>
+            <button type="button" class="keyboard__key">ひ</button>
+            <button type="button" class="keyboard__key">ふ</button>
+            <button type="button" class="keyboard__key">へ</button>
+            <button type="button" class="keyboard__key">ほ</button>
+            <button type="button" class="keyboard__key">ハ</button>
+            <button type="button" class="keyboard__key">ヒ</button>
+            <button type="button" class="keyboard__key">フ</button>
+            <button type="button" class="keyboard__key">へ</button>
+            <button type="button" class="keyboard__key">ホ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">ま</button>
+            <button type="button" class="keyboard__key">み</button>
+            <button type="button" class="keyboard__key">む</button>
+            <button type="button" class="keyboard__key">め</button>
+            <button type="button" class="keyboard__key">も</button>
+            <button type="button" class="keyboard__key">マ</button>
+            <button type="button" class="keyboard__key">ミ</button>
+            <button type="button" class="keyboard__key">ム</button>
+            <button type="button" class="keyboard__key">メ</button>
+            <button type="button" class="keyboard__key">モ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">や</button>
+            <button type="button" class="keyboard__key">ゆ</button>
+            <button type="button" class="keyboard__key">よ</button>
+            <button type="button" class="keyboard__key">ヤ</button>
+            <button type="button" class="keyboard__key">ユ</button>
+            <button type="button" class="keyboard__key">ヨ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">ら</button>
+            <button type="button" class="keyboard__key">り</button>
+            <button type="button" class="keyboard__key">る</button>
+            <button type="button" class="keyboard__key">れ</button>
+            <button type="button" class="keyboard__key">ろ</button>
+            <button type="button" class="keyboard__key">ラ</button>
+            <button type="button" class="keyboard__key">リ</button>
+            <button type="button" class="keyboard__key">ル</button>
+            <button type="button" class="keyboard__key">レ</button>
+            <button type="button" class="keyboard__key">ロ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">わ</button>
+            <button type="button" class="keyboard__key">を</button>
+            <button type="button" class="keyboard__key">ワ</button>
+            <button type="button" class="keyboard__key">ヲ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">が</button>
+            <button type="button" class="keyboard__key">ガ</button>
+            <button type="button" class="keyboard__key">ぎ</button>
+            <button type="button" class="keyboard__key">ギ</button>
+            <button type="button" class="keyboard__key">ぐ</button>
+            <button type="button" class="keyboard__key">グ</button>
+            <button type="button" class="keyboard__key">げ</button>
+            <button type="button" class="keyboard__key">ゲ</button>
+            <button type="button" class="keyboard__key">ご</button>
+            <button type="button" class="keyboard__key">ゴ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">ザ</button>
+            <button type="button" class="keyboard__key">ざ</button>
+            <button type="button" class="keyboard__key">じ</button>
+            <button type="button" class="keyboard__key">ジ</button>
+            <button type="button" class="keyboard__key">ず</button>
+            <button type="button" class="keyboard__key">ズ</button>
+            <button type="button" class="keyboard__key">ぜ</button>
+            <button type="button" class="keyboard__key">ゼ</button>
+            <button type="button" class="keyboard__key">ぞ</button>
+            <button type="button" class="keyboard__key">ゾ</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">だ</button>
+            <button type="button" class="keyboard__key">ぢ</button>
+            <button type="button" class="keyboard__key">づ</button>
+            <button type="button" class="keyboard__key">で</button>
+            <button type="button" class="keyboard__key">ど</button>
+            <button type="button" class="keyboard__key">だ</button>
+            <button type="button" class="keyboard__key">ぢ</button>
+            <button type="button" class="keyboard__key">づ</button>
+            <button type="button" class="keyboard__key">で</button>
+            <button type="button" class="keyboard__key">ど</button>
+        </div>
+        <div>
+            <button type="button" class="keyboard__key">ば</button>
+            <button type="button" class="keyboard__key">び</button>
+            <button type="button" class="keyboard__key">ぶ</button>
+            <button type="button" class="keyboard__key">べ</button>
+            <button type="button" class="keyboard__key">ぼ</button>
+            <button type="button" class="keyboard__key">バ</button>
+            <button type="button" class="keyboard__key">ビ</button>
+            <button type="button" class="keyboard__key">ブ</button>
+            <button type="button" class="keyboard__key">ベ</button>
+            <button type="button" class="keyboard__key">ボ</button>
+        </div>
+            <button type="button" class="keyboard__key">ぴ</button>
+            <button type="button" class="keyboard__key">ぺ</button>
+            <button type="button" class="keyboard__key">ぱ</button>
+            <button type="button" class="keyboard__key">ぷ</button>
+            <button type="button" class="keyboard__key">ぽ</button>
+            <button type="button" class="keyboard__key">パ</button>
+            <button type="button" class="keyboard__key">ピ</button>
+            <button type="button" class="keyboard__key">プ</button>
+            <button type="button" class="keyboard__key">ペ</button>
+            <button type="button" class="keyboard__key">ポ</button>
+        <div>
+            <button type="button" class="keyboard__key">ぁ</button>
+            <button type="button" class="keyboard__key">ぃ</button>
+            <button type="button" class="keyboard__key">ぅ</button>
+            <button type="button" class="keyboard__key">ぇ</button>
+            <button type="button" class="keyboard__key">ぉ</button>
+            <button type="button" class="keyboard__key">ゔ</button>
+            <button type="button" class="keyboard__key">ァ</button>
+            <button type="button" class="keyboard__key">ィ</button>
+            <button type="button" class="keyboard__key">ゥ</button>
+            <button type="button" class="keyboard__key">ェ</button>
+            <button type="button" class="keyboard__key">ォ</button>
+            <button type="button" class="keyboard__key">ヴ</button>
+        </div>
+            <br>
+            <button type="button" class="keyboard__key">・</button>
+            <button type="button" class="keyboard__key">ヽ</button>
+            <button type="button" class="keyboard__key">ヾ</button>
+            <button type="button" class="keyboard__key">ー</button>
+            <button type="button" class="keyboard__key">ゝ</button>
+            <button type="button" class="keyboard__key">ゞ</button>
+            <button type="button" class="keyboard__key">、</button>
+            <button type="button" class="keyboard__key">。</button>
+    ];
+}
